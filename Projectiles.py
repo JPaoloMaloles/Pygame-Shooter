21e0cd91efdd2ff30a659a2f14023388368fa869
Paolo Maloles <jpaolomaloles@gmail.com>
2023-09-06 07:13:29 -0500

    basic enemies chasing

--- Projectiles.py
+++ Projectiles.py
@@ -5,40 +5,39 @@ class ProjectileClass():
         self.position = pygame.Vector2(
             input['x_position'], input['y_position'])
         self.projectile = pygame.Surface((10, 10))
-        self.rectangle = test_surface.get_rect(topleft=(self.position))
+        self.rectangle = input['display_surface'].get_rect(topleft=(self.position))
 
-    def fill(self):
+    def fill(self, display_surface):
         self.projectile.fill(f'{self.projectile_color}')
         display_surface.blit(self.projectile, self.position)
         print(f"projectile rendered at {self.position}")
-        print(f'player position at {player_pos}')
 
-    def chase(self):
+    def chase(self, player_position):
         a = self.position.x
         b = self.position.y
-        print(f'&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&: {player_pos.x}')
+        print(f'&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&: {player_position.x}')
         print(f'&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&: {self.position.x}')
         # print(
-        #     f'&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&: {(player_pos.x - self.position.x)/0.5}')
-        if abs((player_pos.x - self.position.x)/0.5) >= 0.5:
-            print(f'@@@@@@@@@@@@@@@@@@@@@@@@@@ {((a - player_pos.x)/0.5):12f}')
-            if player_pos.x > self.position.x:
+        #     f'&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&: {(player_position.x - self.position.x)/0.5}')
+        if abs((player_position.x - self.position.x)/0.5) >= 0.5:
+            print(f'@@@@@@@@@@@@@@@@@@@@@@@@@@ {((a - player_position.x)/0.5):12f}')
+            if player_position.x > self.position.x:
                 self.position.x = self.position.x + \
-                    ((player_pos.x - self.position.x) /
-                     ((player_pos.x - self.position.x)/0.5))
+                    ((player_position.x - self.position.x) /
+                     ((player_position.x - self.position.x)/0.5))
             else:
                 self.position.x = self.position.x - \
-                    ((player_pos.x - self.position.x) /
-                     ((player_pos.x - self.position.x)/0.5))
-        if abs((player_pos.y - self.position.y)/0.5) >= 0.5:
+                    ((player_position.x - self.position.x) /
+                     ((player_position.x - self.position.x)/0.5))
+        if abs((player_position.y - self.position.y)/0.5) >= 0.5:
             print(
-                f'!!!!!!!!!!!!!!!!!!!!!!!! {((b - player_pos.y)/0.5):12f}')
-            if player_pos.y > self.position.y:
+                f'!!!!!!!!!!!!!!!!!!!!!!!! {((b - player_position.y)/0.5):12f}')
+            if player_position.y > self.position.y:
                 self.position.y = self.position.y + \
-                    ((player_pos.y - self.position.y) /
-                     ((player_pos.y - self.position.y)/0.5))
+                    ((player_position.y - self.position.y) /
+                     ((player_position.y - self.position.y)/0.5))
             else:
                 self.position.y = self.position.y - \
-                    ((player_pos.y - self.position.y) /
-                     ((player_pos.y - self.position.y)/0.5))
+                    ((player_position.y - self.position.y) /
+                     ((player_position.y - self.position.y)/0.5))
     
--- gamecycle.py
+++ gamecycle.py
@@ -3,7 +3,7 @@ from sys import exit
 import random
 import time
 from Player import PlayerClass
-import Projectiles
+from Projectiles import ProjectileClass
 
 
 pygame.init()
@@ -11,6 +11,8 @@ display_surface = pygame.display.set_mode((800,400))
 running = True
 game_running = False
 player_objects = [] #Make sure when ending games to set this to 0
+enemy_objects = []
+enemy_creation_timestamp = []
 clock = pygame.time.Clock()
 dt = 0
 
@@ -39,6 +41,24 @@ def time_display(time_passed):
     display_surface.blit(time_passed_text, (0, 0))
     display_surface.blit(
         fps_text, (display_surface.get_width()-fps_text.get_width(), 0))
+    
+def create_enemy():
+    if time_passed % 3 == 0:
+        if time_passed not in enemy_creation_timestamp:
+            print(f'exact {pygame.time.get_ticks()}')
+            print(f'create projectile-------------------------{time_passed}')
+            print(len(enemy_objects))
+            enemy_objects.append(time_passed)
+            enemy_creation_timestamp.append(time_passed)
+            print(f'pastprojectiles 0: {enemy_creation_timestamp[0]}')
+            enemy_objects[len(enemy_objects) - 1] = ProjectileClass(
+                {'projectile_color': 'red', 'x_position': player_position.x, 'y_position': random.randint(0, display_surface.get_height()), 'display_surface':display_surface})
+            print(
+                f'pastprojectiles AFTER: {enemy_objects[len(enemy_objects) - 1]}')
+            enemy_objects[0].fill(display_surface)
+
+        if len(enemy_creation_timestamp) > 1:
+            enemy_creation_timestamp.pop(0)
 
 # ------------------------------------------------------ game cycle
 while running:
@@ -56,7 +76,7 @@ while running:
                     player_objects[len(player_objects)-1] = PlayerClass({'player_position': player_position, 'player_color':'white', 'player_surface':pygame.Surface((10, 10))})
                     print('player created')
                     game_running = True
-    
+
     while game_running:
         time_passed = round(pygame.time.get_ticks() * 0.001, 1)
         dt = clock.tick(60) / 1000
@@ -67,6 +87,7 @@ while running:
             if event.type ==  pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     player_objects = []
+                    enemy_creation_timestamp = []
                     game_running = False
     # keys = pygame.key.get_pressed()
     # if keys[pygame.K_SPACE]:
@@ -89,7 +110,7 @@ while running:
 
         print(f'@@@@@@@@@@@@@ {player_position.x}')
         print(f'~~~~~~~~~~~~~ {player_position.y}')
-
+        create_enemy()
         # ------------------------------------------------------ rendering graphics
         display_surface.fill('Purple')
         time_display(time_passed)
@@ -97,6 +118,9 @@ while running:
             out_of_bounds(player_position, player.player_surface)
             player.render(display_surface, player_position)
             # display_surface.blit(player.player_surface, player_rect)
+        for enemy in enemy_objects:
+            enemy.chase(player_position)
+            enemy.fill(display_surface)
         pygame.display.flip()
     time_display(time_passed)
     pygame.display.flip()
